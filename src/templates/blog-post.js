9418ff55fbbe527bd04acb682d833e8311234d18
DongHeon Lee <niche3600@gmail.com>
2021-01-02 19:55:38 +0900

    iframelyを使うための設定とコンポーネントを追加

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Iframely from "../components/iframely"
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
@@ -12,6 +13,7 @@ const BlogPostTemplate = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
+      <Iframely />
       <SEO
         title={post.frontmatter.title}
         description={post.frontmatter.description || post.excerpt}
