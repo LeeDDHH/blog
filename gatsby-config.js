c0d4d19fc3f1a63f3f05d0ddd1ff6727c74b292d
DongHeon Lee <niche3600@gmail.com>
2021-01-04 05:48:48 +0900

    外部リンクを常に別タブで開くように設定するプラグインを導入
    gatsby-remark-external-links

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,12 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          {
+            resolve: `gatsby-remark-external-links`,
+            options: {
+              rel: "noopener noreferrer"
+            },
+          }
         ],
       },
     },
