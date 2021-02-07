b9c5f20392b742cf794556acaff55b472f55ea04
dongheon lee <niche3600@gmail.com>
2021-02-07 14:01:09 +0900

    サーチコンソルの参照エラーを修正
    オブジェクト型に渡されていたため、stringに修正

--- src/components/searchConsole.js
+++ src/components/searchConsole.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
-const SearchConsole = (title) => {
+const SearchConsole = ({ title }) => {
   return (
     <Helmet
       title={title}
